a656d21f42cd5fb90157a183cc1156bc9cb48664
Puput <poed.wae@gmail.com>
2024-04-27 01:39:18 +0700

    reponsive max 450,600,logo client,fix page home

--- js/script.js
+++ js/script.js
@@ -31,3 +31,21 @@ btn.addEventListener('click',()=>{
         Body:"Test body"
     }).then(msg=>alert("The email successfully sent"))
 })
+
+
+// const counts = document.querySelectorAll('.count')
+// const speed= 97
+// counts.forEach((counter) => {
+//     function upData(){
+//         const target = Number(counter.getAttribute('data-target'))
+//         const count = Number(counter.innertText)
+//         const inc = target / speed
+//         if(count < target){
+//             counter.innerText = Math.floor(inc + count)
+//             setTimeout(upData, 1)
+//         }else{
+//             counter.innerText = target
+//         }
+//     }
+//     upData()
+// })

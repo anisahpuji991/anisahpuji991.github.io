fdcb1ad3ad95129d4e9c4f4e9ac78f61f41d27c8
Puput <poed.wae@gmail.com>
2024-04-28 01:43:05 +0700

    responsive III

--- js/script.js
+++ js/script.js
@@ -17,35 +17,5 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
-const btn = document.querySelector('#contact_btn');
-const inputs = document.querySelector('form');
-btn.addEventListener('click',()=>{
-    //alert(inputs.elements["contact_email"].value);
-    Email.send({
-        Host:"sandbox.smtp.mailtrap.io",
-        Username:'704d949b130fb0',
-        Password:'2c31fa2b5afe75',
-        To:'frameconsultant@gm.com',
-        From:inputs.elements["contact_email"].value,
-        Subject:inputs.elements["contact_subject"].value,
-        Body:"Test body"
-    }).then(msg=>alert("The email successfully sent"))
-})
 
 
-// const counts = document.querySelectorAll('.count')
-// const speed= 97
-// counts.forEach((counter) => {
-//     function upData(){
-//         const target = Number(counter.getAttribute('data-target'))
-//         const count = Number(counter.innertText)
-//         const inc = target / speed
-//         if(count < target){
-//             counter.innerText = Math.floor(inc + count)
-//             setTimeout(upData, 1)
-//         }else{
-//             counter.innerText = target
-//         }
-//     }
-//     upData()
-// })
